00311bc66ef694837a7e67fecaf715d52f7ec3aa
Nanoo <tempi@mac.com>
2016-10-31 22:00:11 -0400

    Remove commented out code.

--- NeoPixelStar/NeoPixelStar.ino
+++ NeoPixelStar/NeoPixelStar.ino
@@ -61,12 +61,6 @@ void loop() {
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  /*for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-    strip.setPixelColor(
-  }*/
   strip.setPixelColor(6, c);
   strip.show();
   delay(wait);
